5e44f4f23a1a8b52427db5cba38843137ab5e237
Joel Alexander <joel.lewis.alexander@gmail.com>
2022-09-25 19:05:22 -0400

    Make delete script less useless

--- delete.sh
+++ delete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker compose stop
-docker compose rm -v
+docker compose -f docker-compose.yml -f rpc.yml -f sealer.yml -f ssl.yml rm -v
 rm -rf .env network.json package-lock.json creator.json environment.json bootnode.key password.txt genesis.json keystore http.conf
 cd ~
